291650964dc9af9f41833c929569af57139b52bc
Diego Hernandes <diego@hernandev.com>
2016-08-30 01:16:01 -0300

    creating social login env vars

--- config/eloquent-oauth.php
+++ config/eloquent-oauth.php
@@ -1,27 +1,25 @@
 <?php
 
-use App\User;
-
 return [
-    'model' => User::class,
+    'model' => \Codecasts\Domains\Users\User::class,
     'table' => 'oauth_identities',
     'providers' => [
+        'github' => [
+            'client_id' => trim(env('GITHUB_CLIENT_ID')),
+            'client_secret' => trim(env('GITHUB_CLIENT_SECRET')),
+            'redirect_uri' => '',
+            'scope' => ['user:email'],
+        ],
         'facebook' => [
-            'client_id' => '12345678',
-            'client_secret' => 'y0ur53cr374ppk3y',
-            'redirect_uri' => 'https://example.com/your/facebook/redirect',
-            'scope' => [],
+            'client_id' => env('FACEBOOK_CLIENT_ID'),
+            'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+            'redirect_uri' => (env('APP_SECURE', false) ? 'https://': 'http://').env('APP_DOMAIN').'/auth/callback/facebook',
+            'scope' => ['email'],
         ],
         'google' => [
-            'client_id' => '12345678',
-            'client_secret' => 'y0ur53cr374ppk3y',
-            'redirect_uri' => 'https://example.com/your/google/redirect',
-            'scope' => [],
-        ],
-        'github' => [
-            'client_id' => '12345678',
-            'client_secret' => 'y0ur53cr374ppk3y',
-            'redirect_uri' => 'https://example.com/your/github/redirect',
+            'client_id' => env('GOOGLE_CLIENT_ID'),
+            'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+            'redirect_uri' => (env('APP_SECURE', false) ? 'https://': 'http://').env('APP_DOMAIN').'/auth/callback/google',
             'scope' => [],
         ],
     ],
